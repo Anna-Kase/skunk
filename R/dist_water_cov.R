dcf4fc578fbcfe39cc01516973e37934100f675e
Mason Fidino <mfidino@lpzoo.org>
2022-10-12 10:45:01 -0500

    slight code tweaks to calculating distance to water

--- R/dist_water_cov.R
+++ R/dist_water_cov.R
@@ -1,21 +1,26 @@
 
 # Creating a distance to stream covariate
+library(dplyr)
+library(sf)
 
 # source spatial points file
 source("./R/creating_spatial_points.R")
 
 
 # Dowload data from:
-# browseURL("https://clearinghouse.isgs.illinois.edu/data/hydrology/streams-and-shorelines")
+ browseURL("https://clearinghouse.isgs.illinois.edu/data/hydrology/streams-and-shorelines")
 # load water data
 
+water_path <- "../../GIS/water"
 water <- sf::st_read(
-  "../../GIS/water",
+  water_path,
   layer = "IL_Streams_From_100K_DLG_Ln"
 )
 
 # fix any potential issues
-water_data <- sf::st_make_valid(water)
+water_data <- sf::st_make_valid(
+  water
+)
 
 # reproject into the UTM
 water_data <- sf::st_transform(
@@ -26,9 +31,17 @@ water_data <- sf::st_transform(
 # cut down water data to the extent of the study area
 # this is the same extent as the sites shapefile that was 
 # sourced in
-small_water <- sf::st_crop(water_data, xmin=395087, xmax=456196.7,
-                           ymin=4604022, ymax=4678480)
-
+small_water <- sf::st_crop(
+  water_data,
+  xmin=395087,
+  xmax=456196.7,
+  ymin=4604022,
+  ymax=4678480
+)
+# make valid once more after cropping
+small_water <- sf::st_make_valid(
+  small_water
+)
 
 # Plot it to make sure it looks correct
 plot(small_water$geometry)
@@ -39,61 +52,79 @@ plot(sites$geometry, add=TRUE, pch=19, col="forestgreen")
 # only objects that have the designation for being a stream.
 # Interpretation of values on the metadata tab at same URL the data
 # was downloaded from under "Entity_and_Attribute_Information"
-smaller_water <- select(small_water, ENR)
+smaller_water <- dplyr::select(
+  small_water,
+  ENR
+)
 
 smallest_water <- smaller_water %>% 
-  filter(ENR >= 400000) 
+  dplyr::filter(ENR >= 400000) 
 
-# Calculate the distance of each site to the streams
-# This creates a matrix where each row is a site and each
-# column is the distance of that site to the 1026 
-# different streams
-dist <- sf::st_distance(sites, smallest_water)
+# Get the index of the closest stream to each site
+
+closest_streams_index <- sf::st_nearest_feature(
+  sites, 
+  smallest_water
+)
+
+# Do element wise calculations, indexing
+#  smallest_water by closest_streams_index
+site_dist <- sf::st_distance(
+  sites,
+  smallest_water[closest_streams_index,],
+  by_element = TRUE
+)
 
 # Remove the units (it's meters) from the distances
-dist <- units::drop_units(dist)
+site_dist <- units::drop_units(
+  site_dist
+)
 
 # Make it a dataframe
-dist_df <- as.data.frame(dist)
-
-# Create a column that is the minimum value of each row
-# allowing us to isolate the distance of each site to the 
-# nearest stream
-dist_df$water_dist <- apply(dist,1,FUN=min)
-
-# Select out just the column of minimum distance to stream
-# values for each site
-select_dist <- select(dist_df, water_dist)
+dist_df <- data.frame(
+  water_dist = site_dist
+)
 
 # Add site designations back in so this is not just a list
 # of distance values and it can be joined with the other covariates
-select_dist$Site <- sites$Site
+dist_df$Site <- sites$Site
 
 # Add the distance to water covariate to the other
 # scaled covariates
 
-source("./R/scaled_covariates.R")
+if(
+  file.exists(
+    "./data/site_covariates.csv"
+  )
+){
+  my_covariates <- read.csv(
+    "./data/site_covariates.csv"
+  )
+} else {
+  source("./R/scaled_covariates.R")
+}
 
-more_covariates <- inner_join(
-  x= my_covariates,
-  y= select_dist,
-  by = "Site"
-)
+# check if dist column on here, if not add it
+if(!"water_dist" %in% colnames(my_covariates)){
+  more_covariates <- inner_join(
+    x= my_covariates,
+    y= dist_df,
+    by = "Site"
+  )
+  write.csv(
+    more_covariates,
+    "./data/site_covariates.csv",
+    row.names = FALSE
+  )
+}
 
-View(more_covariates)
-
-# Save as .csv file -- this line will override 
-# the covariate file made in the scaled_covariates.R script
-# write.csv(more_covariates, "./data/site_covariates.csv", row.names = FALSE)
-
-
-
-
-
-sites$dist <- select_dist$water_dist
+# add water dist onto here
+sites$dist <- dist_df$water_dist
 
 #Create a function to generate a continuous color palette
-rbPal <- colorRampPalette(c('blue','red'))
+rbPal <- colorRampPalette(
+  c('blue','red')
+)
 
 #This adds a column of color values
 # based on the y values
@@ -102,11 +133,5 @@ sites$col <- rbPal(100)[as.numeric(cut(sites$dist,breaks = 100))]
 plot(small_water$geometry)
 plot(sites$geometry, pch = 20, col = sites$col, cex=2, add=TRUE)
 
-range(sites$dist)
-
-
-site_coords <- read.csv(
-  "./data/full_capture_history.csv"
-)
 
 
