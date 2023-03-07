107ca91297cf500e7f6340666625ca82aaacbd3e
Anna Kase <89794040+Anna-Kase@users.noreply.github.com>
2023-03-07 08:30:08 -0600

    plots

--- R/occupancy.R
+++ R/occupancy.R
@@ -62,6 +62,10 @@ occ1_summary <- apply(
   probs = c(0.025,0.5,0.975)
 )
 
+occ1_int <- occ1_summary[,1]
+occ1_urb <- occ1_summary[,2]
+occ1_water <- occ1_summary[,3]
+occ1_open <- occ1_summary[,4]
 
 occ2 <- dprob_neigh2 / (dprob_neigh2 + (1 - phi))
 occ2_summary <- apply(
@@ -72,6 +76,12 @@ occ2_summary <- apply(
 )
 
 
+occ2_int <- occ2_summary[,1]
+occ2_urb <- occ2_summary[,2]
+occ2_water <- occ2_summary[,3]
+occ2_open <- occ2_summary[,4]
+
+
 occ3 <- dprob_neigh3 / (dprob_neigh3 + (1 - phi))
 occ3_summary <- apply(
   occ3,
@@ -79,6 +89,17 @@ occ3_summary <- apply(
   quantile,
   probs = c(0.025,0.5,0.975)
 )
+
+occ3_int <- occ3_summary[,1]
+occ3_urb <- occ3_summary[,2]
+occ3_water <- occ3_summary[,3]
+occ3_open <- occ3_summary[,4]
+
+
+occ_int <- cbind(occ1_int, occ2_int, occ3_int)
+occ_urb <- cbind(occ1_urb, occ2_urb, occ3_urb)
+occ_water <- cbind(occ1_water, occ2_water, occ3_water)
+occ_open <- cbind(occ1_open, occ2_open, occ3_open)
 
 
 occ1_fall <- dprob_neigh1_fall / (dprob_neigh1_fall + (1 - phi))
@@ -90,6 +111,12 @@ occ1_summary_fall <- apply(
 )
 
 
+occ1_int_fall <- occ1_summary_fall[,1]
+occ1_urb_fall <- occ1_summary_fall[,2]
+occ1_water_fall <- occ1_summary_fall[,3]
+occ1_open_fall <- occ1_summary_fall[,4]
+
+
 occ2_fall <- dprob_neigh2_fall / (dprob_neigh2_fall + (1 - phi))
 occ2_summary_fall <- apply(
   occ2_fall,
@@ -97,6 +124,12 @@ occ2_summary_fall <- apply(
   quantile,
   probs = c(0.025,0.5,0.975)
 )
+
+
+occ2_int_fall <- occ2_summary_fall[,1]
+occ2_urb_fall <- occ2_summary_fall[,2]
+occ2_water_fall <- occ2_summary_fall[,3]
+occ2_open_fall <- occ2_summary_fall[,4]
 
 
 occ3_fall <- dprob_neigh3_fall / (dprob_neigh3_fall + (1 - phi))
@@ -108,10 +141,23 @@ occ3_summary_fall <- apply(
 )
 
 
+occ3_int_fall <- occ3_summary_fall[,1]
+occ3_urb_fall <- occ3_summary_fall[,2]
+occ3_water_fall <- occ3_summary_fall[,3]
+occ3_open_fall <- occ3_summary_fall[,4]
+
+
+
+occ_int_fall <- cbind(occ1_int_fall, occ2_int_fall, occ3_int_fall)
+occ_urb_fall <- cbind(occ1_urb_fall, occ2_urb_fall, occ3_urb_fall)
+occ_water_fall <- cbind(occ1_water_fall, occ2_water_fall, occ3_water_fall)
+occ_open_fall <- cbind(occ1_open_fall, occ2_open_fall, occ3_open_fall)
+
+
 
 # plot
 
-windows(4, 3) 
+windows(7, 3) 
 
 # tiff(
 #   "./plots/delta_figure.tiff",
@@ -125,8 +171,23 @@ windows(4, 3)
 
 { 
   # blank plot
-  par(mar = c(5, 2, 0.5, 0.5), oma = c(0, 4, 0, 0), lend = 1)
-  bbplot::blank(xlim = c(0.5, 3.5), ylim = c(0, 0.8), bty = "l")
+  m <- matrix(1:3, ncol = 3)
+  layout(m)
+  
+  par(mar = c(5, 2, 0.5, 0.5), oma = c(0, 5, 0, 0), lend = 1)
+  u <- par("usr")
+  
+  # urbanization plot A
+  
+  bbplot::blank(xlim = c(0.5, 3.5), ylim = c(0, 1), bty = "l")
+  
+  text(
+    x = u[1] +0.1,
+    y = u[4] - 0.1,
+    labels = "A)",
+    cex = 1.2,
+    pos = 4
+  )
   
   # axis hash marks
   bbplot::axis_blank(1, at = seq(1,3, by = 1))
@@ -147,21 +208,21 @@ windows(4, 3)
   for(i in 1:3){
     lines(
       x = rep(i-0.125,2),
-      y = occ1_summary[-2,i],
+      y = occ_urb_fall[-2,i],
       col ="purple",
       lwd = 3
     )
   }
   points(
     x = c(1:3)-0.125,
-    y = occ1_summary[2,],
+    y = occ_urb_fall[2,],
     pch = 18,
     col = "black",
     cex = 2
   )
   points(
     x = c(1:3)-0.125,
-    y = delta_prob_fall[2,],
+    y = occ_urb_fall[2,],
     pch = 18,
     col = "purple",
     cex = 1.5
@@ -170,21 +231,21 @@ windows(4, 3)
   for(i in 1:3){
     lines(
       x = rep(i+0.125,2),
-      y = delta_prob[-2,i],
+      y = occ_urb[-2,i],
       col ="darkgreen",
       lwd = 3
     )
   }
   points(
     x = c(1:3)+0.125,
-    y = delta_prob[2,],
+    y = occ_urb[2,],
     pch = 19,
     col = "black",
     cex = 1.5
   )
   points(
     x = c(1:3)+0.125,
-    y = delta_prob[2,],
+    y = occ_urb[2,],
     pch = 19,
     col = "darkgreen",
     cex = 1
@@ -202,6 +263,156 @@ windows(4, 3)
     pt.cex = c(1.5,1)
   )
   
+  
+  # wateranization plot B
+  
+  bbplot::blank(xlim = c(0.5, 3.5), ylim = c(0, 1), bty = "l")
+  u <- par("usr")
+  
+  text(
+    x = u[1] + (u[1] * (1-0.93)),
+    y = u[4] - 0.1,
+    labels = "B)",
+    cex = 1.2,
+    pos = 4
+  )
+  
+  # axis hash marks
+  bbplot::axis_blank(1, at = seq(1,3, by = 1))
+  bbplot::axis_blank(2)
+  
+  # axis hash mark labels
+  bbplot::axis_text(text = seq(1,3,by=1), line = 0.9,
+                    side = 1, at = seq(1,3,1))
+  bbplot::axis_text(side = 2, las = 1, line = 0.4)
+  
+  # axis labels
+  bbplot::axis_text("Number of neighboring sites \n with striped skunk", 
+                    side = 1, line = 3)
+  bbplot::axis_text("Pr(Occupancy)", side = 2, outer = TRUE, at = 0.65)
+  
+  # layer on data
+  
+  for(i in 1:3){
+    lines(
+      x = rep(i-0.125,2),
+      y = occ_water_fall[-2,i],
+      col ="purple",
+      lwd = 3
+    )
+  }
+  points(
+    x = c(1:3)-0.125,
+    y = occ_water_fall[2,],
+    pch = 18,
+    col = "black",
+    cex = 2
+  )
+  points(
+    x = c(1:3)-0.125,
+    y = occ_water_fall[2,],
+    pch = 18,
+    col = "purple",
+    cex = 1.5
+  )
+  
+  for(i in 1:3){
+    lines(
+      x = rep(i+0.125,2),
+      y = occ_water[-2,i],
+      col ="darkgreen",
+      lwd = 3
+    )
+  }
+  points(
+    x = c(1:3)+0.125,
+    y = occ_water[2,],
+    pch = 19,
+    col = "black",
+    cex = 1.5
+  )
+  points(
+    x = c(1:3)+0.125,
+    y = occ_water[2,],
+    pch = 19,
+    col = "darkgreen",
+    cex = 1
+  )
+  
+  # openanization plot B
+  
+  bbplot::blank(xlim = c(0.5, 3.5), ylim = c(0, 1), bty = "l")
+  u <- par("usr")
+  
+  text(
+    x = u[1] + (u[1] * (1-0.93)),
+    y = u[4] - 0.1,
+    labels = "C)",
+    cex = 1.2,
+    pos = 4
+  )
+  
+  # axis hash marks
+  bbplot::axis_blank(1, at = seq(1,3, by = 1))
+  bbplot::axis_blank(2)
+  
+  # axis hash mark labels
+  bbplot::axis_text(text = seq(1,3,by=1), line = 0.9,
+                    side = 1, at = seq(1,3,1))
+  bbplot::axis_text(side = 2, las = 1, line = 0.4)
+  
+  # axis labels
+  bbplot::axis_text("Number of neighboring sites \n with striped skunk", 
+                    side = 1, line = 3)
+  bbplot::axis_text("Pr(Occupancy)", side = 2, outer = TRUE, at = 0.65)
+  
+  # layer on data
+  
+  for(i in 1:3){
+    lines(
+      x = rep(i-0.125,2),
+      y = occ_open_fall[-2,i],
+      col ="purple",
+      lwd = 3
+    )
+  }
+  points(
+    x = c(1:3)-0.125,
+    y = occ_open_fall[2,],
+    pch = 18,
+    col = "black",
+    cex = 2
+  )
+  points(
+    x = c(1:3)-0.125,
+    y = occ_open_fall[2,],
+    pch = 18,
+    col = "purple",
+    cex = 1.5
+  )
+  
+  for(i in 1:3){
+    lines(
+      x = rep(i+0.125,2),
+      y = occ_open[-2,i],
+      col ="darkgreen",
+      lwd = 3
+    )
+  }
+  points(
+    x = c(1:3)+0.125,
+    y = occ_open[2,],
+    pch = 19,
+    col = "black",
+    cex = 1.5
+  )
+  points(
+    x = c(1:3)+0.125,
+    y = occ_open[2,],
+    pch = 19,
+    col = "darkgreen",
+    cex = 1
+  )
 }
 
 dev.off()
--- R/occupancy_simulation_example.R
+++ R/occupancy_simulation_example.R
@@ -16,6 +16,83 @@ loc_count <- array(
   dim = c(100,100, 12)
 )
 
+
+one_loc <- locs[,,1]
+
+my_neighs <- vector("list", length = prod(dim(one_loc)))
+
+for(i in 1:nrow(one_loc)){
+  for(j in 1:ncol(one_loc)){
+    tmp_neighs <- matrix(
+      c(
+        i + 1, j,
+        i, j-1,
+        i - 1, j,
+        i, j+1
+      ),
+      ncol = 2,
+      byrow = TRUE
+    )
+    to_go <- which(
+      tmp_neighs < 1 | tmp_neighs > 100,
+      arr.ind = TRUE
+    )
+    if(nrow(to_go)>0){
+      tmp_neighs <- tmp_neighs[-to_go[,1],]
+    }
+    tmp_neighs <- c(i - 1, i +1, j - 1, j +1 )
+    tmp_neigh_locs
+  }
+}
+
+
+
+for(i in 1:nsim){  
+  # initial occupancy
+  tmp_psi <- dm %*% psi
+  tmp_psi <- plogis(tmp_psi)
+  # sample across the landscape
+  locs[,,1] <- rbinom(
+    nrow(tmp_psi),
+    1,
+    tmp_psi
+  )
+  
+  for(j in 2:12){
+    zeta[i,j-1] <- 
+      locs[,,j-1] %*% tmp_neighs
+    
+    tmp_gamma <- plogis(dm %*% gamma)
+    tmp_phi <- plogis(dm %*% phi)
+    
+    tmp_delta <- 1 - exp(
+      (locs[,,j-1] * tmp_neighs) %*%
+        log(1 - (plogis(dm %*% delta))) #***
+    )
+  }  
+}
+
+# ***This is the part where I keep going back and forth if we need 
+  # more specificity on # of neighbors. Originally, the indicator term
+  # took on a zero or a one that would turn this entire delta term
+  # on and off. So it would make sense that we push this through the
+  # same equation from the original model where it wasn't explicit how
+  # many neighbors a site had, and we used the delta output in 
+  # conjunction with a design matrix to obtain the delta values used
+  # for plotting. Therefore, it seems like that process should be repeated
+  # here for the simulations.
+  # One of my original ideas was that we would need to have a bigger
+  # if loop that would use a specific delta probability based on the
+  # number of neighbors a site had. But that no longer seems correct
+  # the more I have thought about this. 
+
+
+
+
+
+
+
+
 # covariates -- this case we are using the 
 # intercept and open space
 dm <- cbind(1, rnorm(100*100))
@@ -44,6 +121,7 @@ for(i in 1:nsim){
     
     tmp_gamma <- plogis(dm %*% gamma)
     tmp_phi <- plogis(dm %*% phi)
+    
     tmp_delta <- 1 - exp(
       (locs[,,j-1] * tmp_neighs) %*%
         log(1 - (plogis(dm %*% delta)))
