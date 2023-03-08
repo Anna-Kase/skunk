4fa50522bb8e3cfbdd73c6609c456be96ae704b3
Mason Fidino <mfidino@lpzoo.org>
2023-03-08 13:41:17 -0600

    some plotting stuff by Anna and I

---
+++ R/plot_simulation.R
@@ -0,0 +1,97 @@
+library(sf)
+library(prettymapr)
+sf::sf_use_s2(FALSE)
+library(bbplot)
+
+plot_array <- readRDS("./data/phi_gamma_delta_by_season.RDS")
+
+oz <- read.csv(
+  "./data/original_z_sim_values.csv"
+)
+nsamp <- 10000
+
+# average occupancy each site and season
+mu_z <- oz/(nsamp)
+
+# overall occupancy throughout study
+gmu_z <- rowMeans(mu_z)
+
+# variation in occupancy across seasons
+occ_sd <- apply(mu_z, 1, sd)
+
+gr <- read.csv(
+  "./data/raw_sim_covariates/point_locs.csv"
+)
+
+gr <- sf::st_as_sf(
+  gr,
+  coords = c("x","y"),
+  crs = 32616
+)
+gr$occ_prob <- gmu_z
+gr$occ_sd <- occ_sd
+
+windows()
+
+
+
+
+# Changes in phi, gamma, delta over simulated time
+
+
+windows(7, 3)
+
+{
+  par(mar = c(3, 4, 0.5, 10), oma = c(0,0,0,0), lend = 1)
+  bbplot::blank(xlim = c(1,11), ylim = c(0, 0.6), bty = "l")
+  
+  bbplot::axis_blank(1, at = seq(1, 11, by = 1))
+  bbplot::axis_blank(2)
+  bbplot::axis_text(text = seq(1, 11, 1), side = 1, line = 0.3,
+                    at = seq(1, 11, 1))
+  bbplot::axis_text(side = 2, las = 1, line = 0.4)
+  bbplot::axis_text("Season", side = 1, line = 1.5, cex = 1.2)
+  bbplot::axis_text("Proportion sites", side = 2, at = 0.3, 
+                    line = 2.25, cex = 1.2)
+  my_col <- c("purple", "black", "green")
+  my_jiggle <- c(-0.125,0, 0.125)
+  my_pch <- c(21,22,23)
+  for(i in 1:3){
+    for(t in 1:11){
+      lines(
+        x = rep(t + my_jiggle[i],2),
+        y = plot_array[-2,i,t],
+        lwd = 2,
+        col = my_col[i]
+      )
+    }
+    points(
+      x = c(1:11) + my_jiggle[i],
+      y = plot_array[2,i,],
+      bg = my_col[i],
+      cex = 1.2,
+      pch = my_pch[i]
+    )
+    
+  }
+  par(xpd = NA)
+  legend(
+    x = 11.5,
+    y = 0.5,
+    legend = c(
+      "Persistence",
+      "Random colonization",
+      "Local colonization"
+    ),
+    pch = my_pch,
+    pt.bg = my_col,
+    pt.cex = 1.2,
+    bty = "n",
+    y.intersp = 1.5
+  )
+  
+}
+
+
+
+
--- R/simulation_model.R
+++ R/simulation_model.R
@@ -1,6 +1,4 @@
-
 library(dplyr)
-library(parallel)
 
 locs <- readRDS("./data/simulation_neighbors.RDS")
 
@@ -10,7 +8,7 @@ output <- readRDS("./skunk_rds/spatial_covariates_fall2.RDS")
 source("./R/mcmc_functions.R")
 output <- do.call("rbind", output)
 set.seed(89)
-nsamp <- 10
+nsamp <- 100
 output <- output[sample(1:nrow(output), nsamp),]
 mc <- split_mcmc(output)
 
@@ -37,7 +35,7 @@ fall_vec <- c(0, 0, 1, 0, 0, 0, 1, 0, 0,
               0, 1, 0, 0, 0, 1)
 
 oz <- array(0, dim = c(nsite, nseason))
-plot_array <- array(0, dim=c(nsamp, 3, nseason))
+plot_array <- array(0, dim=c(nsamp, 3, nseason-1))
 
 pb <- txtProgressBar(max = nsamp)
 for(s in 1:nsamp){
@@ -67,14 +65,14 @@ for(s in 1:nsamp){
     if(length(phi_sites)>0){
       tmp_phi <- dm_covs[phi_sites,] %*% mc$phi_beta[s,]
       my_prob[phi_sites] <- plogis(tmp_phi)
-      
+      plot_array[s,1,j-1] <- length(phi_sites)
     }
     gamma_sites <- which(z[,j-1] == 0 & !has_n)
       if(length(gamma_sites)>0){
         tmp_gamma <- dm_covs[gamma_sites,] %*% mc$gamma_beta[s,] + 
           mc$gamma_fall[s,] * fall_vec[j]
         my_prob[gamma_sites] <- plogis(tmp_gamma)
-      
+        plot_array[s,2,j-1] <- length(gamma_sites)
       }
     
     delta_sites <- which(z[,j-1] == 0 & has_n)
@@ -94,7 +92,7 @@ for(s in 1:nsamp){
               }
         )
         my_prob[delta_sites] <- tmp_delta
-      
+        plot_array[s,3,j-1] <- length(delta_sites)
       }
       
     z[,j] <- rbinom(
@@ -102,75 +100,21 @@ for(s in 1:nsamp){
       1,
       my_prob
     )
-    
-    plot_array[s,1,j] <- length(my_prob[phi_sites])
-    plot_array[s,2,j] <- length(my_prob[gamma_sites])
-    plot_array[s,3,j] <- length(my_prob[delta_sites])
+
+
     
     }  
   oz <- oz + z
 
 }
 
+plot_array <- apply(
+  plot_array / nrow(covs), 
+  c(2,3),
+  quantile,
+  probs = c(0.025,0.5,0.975)
+)
+
+
 #write.csv(oz, "./data/original_z_sim_values.csv")
-
-# average occupancy each site and season
-mu_z <- oz/(nsamp)
-
-# overall occupancy throughout study
-gmu_z <- rowMeans(mu_z)
-
-# variation in occupancy across seasons
-occ_sd <- apply(mu_z, 1, sd)
-
-gr <- read.csv(
-  "./data/raw_sim_covariates/point_locs.csv"
-)
-
-library(sf)
-sf::sf_use_s2(FALSE)
-gr <- sf::st_as_sf(
-  gr,
-  coords = c("x","y"),
-  crs = 32616
-)
-gr$occ_prob <- gmu_z
-gr$occ_sd <- occ_sd
-
-plot(gr["occ_prob"], pch = 15)
-plot(gr["occ_sd"], pch = 15)
-
-
-
-# Changes in phi, gamma, delta over simulated time
-
-library(bbplot)
-
-windows(7, 3)
-
-{
-  par(mar = c(5, 2, 0.5, 0.5), oma = c(0, 5, 0, 0), lend = 1)
-  bbplot::blank(xlim = c(1,12), ylim = c(0, 1), bty = "l")
-  
-  bbplot::axis_blank(1, at = seq(1, 12, by = 1))
-  bbplot::axis_blank(2)
-  bbplot::axis_text(text = seq(1, 12, 1), side = 1, line = 0.9,
-                    at = seq(1, 12, 1))
-  bbplot::axis_text(side = 2, las = 1, line = 0.4)
-  bbplot::axis_text("Simulated Season", side = 1, line = 2.5)
-  bbplot::axis_text("Proportion Sites", side = 2, outer = TRUE, at = 0.6, 
-                    line = 2)
-  
-  points(
-    x = c(1:12)-0.125,
-    y = ((sum(plot_array[1:nsamp,1,1:12]))/nsite)/nsamp,
-    pch = 18,
-    col = "black",
-    cex = 2
-  )
-}
-
-
-
-
-  
+saveRDS(plot_array, "./data/phi_gamma_delta_by_season.RDS")
